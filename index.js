1ca63927087591d51c2b6f0bd7afed1c47561c06
5wintaek <elqkddl@naver.com>
2023-01-14 11:06:46 +0900

    1/14 useState

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-function 바깥함수(){
-  const 변수 = 0 
+const useState = (초깃값) => {
+	let 상태 = 초깃값
 
-  function 안쪽함수(){
-    console.log(변수)
-    debugger
-  }
+	function 읽기() {
+		return 상태
+	}
 
-  return 안쪽함수 
+	function 쓰기(새값) {
+		상태 = 새값
+	}
+
+	return [읽기, 쓰기]
 }
+const [읽기, 쓰기] = useState(0)
+// const 읽기 = 배열[0]
+// const 쓰기 = 배열[1]
+console.log(읽기()) // 0
+쓰기(읽기() + 1)
+console.log(읽기()) // 1
 
-const 안쪽함수 = 바깥함수()
-
-안쪽함수()
-
-
-
+// debugger
+// a[0] // 읽기
+// a[1] // 쓰기
